17dfe81f4b9c5e0a9f4af8387534baec4eeac4b1
Patrick Nguyen <patar.nguyen@gmail.com>
2022-08-08 12:11:31 -0400

    added project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,6 +7,7 @@ import img4 from '../images/Quiztacular.png';
 import img5 from '../images/onlyFriendZ.png';
 import img6 from '../images/Create.jpg';
 import img7 from '../images/TinyApp.png';
+import img8 from '../images/list.png';
 const Project = () => {
 
   const projects = [
@@ -29,8 +30,18 @@ const Project = () => {
         <h1>Projects</h1>
       </div>
 
+      <div className="container" onClick={(e) => {  e.preventDefault(); window.open('https://github.com/patar-nguyen/rn-todo_list')}}>
+        <img className="img" src={img8} width="350px" height="300px" alt="todo"/>
+        <div className="overlay">
+          <div className="content">
+            <div className="title">Todo list</div>
+            <p className="description">React Native application that creates a todo list</p>
+          </div>
+        </div>
+      </div>
+
       <div className="container" onClick={(e) => {  e.preventDefault(); window.open('https://github.com/patar-nguyen/wumboflix')}}>
-        <img className="img" src={img3} width="350px" height="300px" alt="tweeter"/>
+        <img className="img" src={img3} width="350px" height="300px" alt="wumboflix"/>
         <div className="overlay">
           <div className="content">
             <div className="title">Wumboflix</div>
@@ -40,7 +51,7 @@ const Project = () => {
       </div>
 
       <div className="container" onClick={(e) => {  e.preventDefault(); window.open('https://github.com/patar-nguyen/note_list')}}>
-        <img className="img" src={img2} width="350px" height="300px" alt="tweeter"/>
+        <img className="img" src={img2} width="350px" height="300px" alt="notes"/>
         <div className="overlay">
           <div className="content">
             <div className="title">Notes</div>
@@ -50,7 +61,7 @@ const Project = () => {
       </div>
 
       <div className="container" onClick={(e) => {  e.preventDefault(); window.open('https://github.com/crocka/onlyFriendZ')}}>
-        <img className="img" src={img5} width="350px" height="300px" alt="tweeter"/>
+        <img className="img" src={img5} width="350px" height="300px" alt="onlyfriendz"/>
         <div className="overlay">
           <div className="content">
             <div className="title">OnlyFriendZ</div>
@@ -60,7 +71,7 @@ const Project = () => {
       </div>
       
       <div className="container" onClick={(e) => {  e.preventDefault(); window.open('https://github.com/crocka/Quiztacular')}}>
-        <img className="img" src={img4} width="350px" height="300px" alt="tweeter"/>
+        <img className="img" src={img4} width="350px" height="300px" alt="quiztacular"/>
         <div className="overlay">
           <div className="content">
             <div className="title">Quiztacular</div>
@@ -70,7 +81,7 @@ const Project = () => {
       </div>
 
       <div className="container" onClick={(e) => {  e.preventDefault(); window.open('https://github.com/patar-nguyen/scheduler')}}>
-        <img className="img" src={img6} width="350px" height="300px" alt="tweeter"/>
+        <img className="img" src={img6} width="350px" height="300px" alt="scheduler"/>
         <div className="overlay">
           <div className="content">
             <div className="title">Scheduler</div>
@@ -90,7 +101,7 @@ const Project = () => {
       </div>
 
       <div className="container" onClick={(e) => {  e.preventDefault(); window.open('https://github.com/patar-nguyen/tinyapp')}}>
-        <img className="img" src={img7} width="350px" height="300px" alt="tweeter"/>
+        <img className="img" src={img7} width="350px" height="300px" alt="tinyapp"/>
         <div className="overlay">
           <div className="content">
             <div className="title">Tiny App</div>
